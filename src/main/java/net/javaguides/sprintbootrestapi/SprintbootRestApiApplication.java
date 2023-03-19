751e769c2f4cda6fc75f2b3c1ffaf1ad7326696c
rixy-ynxy <riu_tokiwa@yunixy.co.jp>
2023-03-19 20:22:04 +0900

    first commit

---
+++ src/main/java/net/javaguides/sprintbootrestapi/SprintbootRestApiApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.sprintbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootRestApiApplication.class, args);
+	}
+
+}
