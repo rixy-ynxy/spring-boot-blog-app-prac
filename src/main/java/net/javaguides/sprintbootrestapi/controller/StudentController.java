261a2841dbcbbbc56281665c6f511ca6172c777e
rixy-ynxy <riu_tokiwa@yunixy.co.jp>
2023-03-26 14:05:50 +0900

    YX-20 query add

--- src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
@@ -4,6 +4,7 @@ import net.javaguides.sprintbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -35,6 +36,14 @@ public class StudentController {
     @PathVariable("first-name") String firstName,
     @PathVariable("last-name") String lastName) {
     return new Student(studentId, firstName, lastName);
+  }
+  //http://localhost:8080/students/query?id=1&firstName=John&lastName=Doe
+  @GetMapping("students/query")
+  public Student studentRequestVariable(
+    @RequestParam int id,
+    @RequestParam String firstName,
+    @RequestParam String lastName) {
+    return new Student(id, firstName, lastName);
   }
 }
 
