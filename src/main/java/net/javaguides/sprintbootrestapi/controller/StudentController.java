572cbb0de6acbfc3d4e3476bd377b86ff611cd40
rixy-ynxy <riu_tokiwa@yunixy.co.jp>
2023-03-26 13:48:15 +0900

    YX-20 add PathVariable

--- src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
@@ -3,6 +3,8 @@ package net.javaguides.sprintbootrestapi.controller;
 import net.javaguides.sprintbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -26,6 +28,13 @@ public class StudentController {
     students.add(new Student(3, "Ram", "Angrea"));
     students.add(new Student(4, "Sanjay", "Kumar"));
     return students;
+  }
+  @GetMapping("students/{id}/{first-name}/{last-name}")
+  public Student studentPathVariable(
+    @PathVariable("id") int studentId,
+    @PathVariable("first-name") String firstName,
+    @PathVariable("last-name") String lastName) {
+    return new Student(studentId, firstName, lastName);
   }
 }
 
