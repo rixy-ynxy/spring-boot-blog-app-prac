8cfc2c341659b2395dd4548fd7e71aadcc2e88bf
rixy-ynxy <riu_tokiwa@yunixy.co.jp>
2023-03-26 13:09:25 +0900

    YX-20 get student student list

--- src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
@@ -3,6 +3,8 @@ package net.javaguides.sprintbootrestapi.controller;
 import net.javaguides.sprintbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import java.util.ArrayList;
 
 
 @RestController
@@ -16,4 +18,16 @@ public class StudentController {
     );
     return student;
   }
+  @GetMapping("students")
+  public List<Student> getStudents() {
+    List<Student> students = new ArrayList<>();
+    students.add(new Student(1, "John", "Doe"));
+    students.add(new Student(2, "Umesh", "Doe"));
+    students.add(new Student(3, "Ram", "Angrea"));
+    students.add(new Student(4, "Sanjay", "Kumar"));
+    return students;
+
+
+  }
 }
+
