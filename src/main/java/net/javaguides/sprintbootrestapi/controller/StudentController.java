7b80902f692203c39223171a0dc60e7034cd06b9
rixy-ynxy <riu_tokiwa@yunixy.co.jp>
2023-03-26 14:25:25 +0900

    YX-20 post response-status method add

--- src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/sprintbootrestapi/controller/StudentController.java
@@ -4,7 +4,11 @@ import net.javaguides.sprintbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -44,6 +48,16 @@ public class StudentController {
     @RequestParam String firstName,
     @RequestParam String lastName) {
     return new Student(id, firstName, lastName);
+  }
+  // Sprign boot REST API that hancles HTTP POST Request
+  // @PostMapping and @RequestBody
+  @PostMapping("students/create")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Student createStudent(@RequestBody Student student) {
+    System.out.println(student.getId());
+    System.out.println(student.getFirstName());
+    System.out.println(student.getLastName());
+    return student;
   }
 }
 
